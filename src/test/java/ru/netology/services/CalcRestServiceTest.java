ad9974cd156f550ad1fa2009ab89d940d018d14f
Valentina.Pro <verano86@mail.ru>
2024-03-24 13:16:34 +0300

    Adding Csv

--- src/test/java/ru/netology/services/CalcRestServiceTest.java
+++ src/test/java/ru/netology/services/CalcRestServiceTest.java
@@ -1,27 +1,27 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class CalcRestServiceTest {
 
-    @Test
-    public void income10Expenses3Threshold20() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/rest.csv")
+    public void income10Expenses3Threshold20(int expected, int income, int expenses, int threshold) {
         CalcRestService service = new CalcRestService();
-        int expected = 3;
-        int actual = service.calculate(10_000, 3_000, 20_000);
+//        int expected = 3;
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
-
-    @Test
-    public void income100Expenses60Threshold150() {
-        CalcRestService service = new CalcRestService();
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
-
-        Assertions.assertEquals(expected, actual);
-    }
+//
+//    @Test
+//    public void income100Expenses60Threshold150() {
+//        CalcRestService service = new CalcRestService();
+//        int expected = 2;
+//        int actual = service.calculate(100_000, 60_000, 150_000);
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
